e77db0f5718ce3ddd3f222bc76a5cc0ae0e5c8af
tylergoodman <tylergoodman@users.noreply.github.com>
2016-02-05 15:11:21 -0800

    static gh ver

--- platforms/android/assets/www/js/index.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-import 'babel-polyfill';
-import React from 'react';
-import { render } from 'react-dom';
-import HauntedMansionTracker from './components/HauntedMansionTracker';
-import character_data from './character_data';
-
-render(<HauntedMansionTracker data={character_data} />, document.getElementById('app'));
--- readme.md
+++
@@ -1,1 +0,0 @@
-Haunted Mansion Tracker
